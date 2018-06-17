e7b6483a091ae94af57a9e3daee16628a3006b5b
Santiago Quiroga <san.quit@hotmail.com>
2018-06-17 19:53:05 -0400

    last parts of the tango with django tutorial free version

--- t_w_d_project/rango/models.py
+++ t_w_d_project/rango/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
 class Category(models.Model):
-	"""This is the model for the categories that will be stored in the Rango app"""
+	"""This is the model for the categories that will be stored in the 
+	Rango app"""
 	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
 
+	class Meta:
+		verbose_name_plural = 'categories'
+		
 	def __str__(self):
 		return self.name
 
 class Page(models.Model):
-	"""This is the model for the pages that will belong that at least one category 
-	and will be shown in the Rango app"""
+	"""This is the model for the pages that will belong that at least one 
+	category and will be shown in the Rango app"""
 	category = models.ForeignKey(Category)
 	title = models.CharField(max_length=128)
 	url = models.URLField()
